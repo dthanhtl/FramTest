92a1448f90bc2175f917d495499a7e7083e5267b
dthanhtl <dthanhtl@gmail.com>
2016-07-07 14:07:05 +0700

    init project

---
+++ FramTest/FramTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FramTest
+//
+//  Created by Thanh Tran on 7/7/16.
+//  Copyright © 2016 ThanhTran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
